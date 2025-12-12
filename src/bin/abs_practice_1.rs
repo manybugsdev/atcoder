0f2238b94435b711821296cd6842117dbde9759b
Manybugs <me@manybugs.dev>
2025-12-12 16:40:02 +0900

    add

---
+++ src/bin/abs_practice_1.rs
@@ -0,0 +1,10 @@
+use std::io::*;
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let [a, b, c, s] = input.split_whitespace().collect::<Vec<_>>()[..] else {
+        panic!()
+    };
+    let [a, b, c] = [a, b, c].map(|v| v.parse::<i64>().unwrap());
+    println!("{} {}", a + b + c, s)
+}
